382a90d9430b0452c25eb39c7e587fd667cae4b9
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2020-09-03 23:28:34 +0900

    Add .gitignore, test files These results are the same as original script by ChoF.

--- test.sh
+++ test.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
+IN=test
+OUT=output
 
-python3 dviasm.py test/hello.dvi
-python3 dviasm.py test/hello-ja.dvi
-python3 dviasm.py -p test/hello-ja-iso2022-jp.dvi
+mkdir -p OUTPUT
+set -x
+
+# tb89cho: dump -> DVI
+python3 dviasm.py $IN/hello.dump -o $OUT/hello.dump.dvi
+cmp $OUT/hello.dump.dvi $IN/hello.dump.dvi || exit 1
+
+# simple: DVI -> dump
+python3 dviasm.py $IN/test.dvi >$OUT/test.dump
+diff $OUT/test.dump $IN/test.dump || exit 1
+python3 dviasm.py $IN/test-ja.dvi >$OUT/test-ja.dump
+diff $OUT/test-ja.dump $IN/test-ja.dump || exit 1
+python3 dviasm.py -p $IN/test-ja-p.dvi >$OUT/test-ja-p.dump
+diff $OUT/test-ja-p.dump $IN/test-ja-p.dump || exit 1
+
+# simple: dump -> DVI
+python3 dviasm.py -o $OUT/test.dump.dvi $IN/test.dump
+cmp $OUT/test.dump.dvi $IN/test.dump.dvi || exit 1
+python3 dviasm.py -o $OUT/test-ja.dump.dvi $IN/test-ja.dump
+cmp $OUT/test-ja.dump.dvi $IN/test-ja.dump.dvi || exit 1
+python3 dviasm.py -p -o $OUT/test-ja-p.dump.dvi $IN/test-ja-p.dump
+cmp $OUT/test-ja-p.dump.dvi $IN/test-ja-p.dump.dvi || exit 1
+
+# finish
+echo success!
