4313fd9e74729009c904c2d7b1e6307b4104dadc
Hironobu Yamashita <h.y.acetaminophen@gmail.com>
2020-09-10 19:15:51 +0900

    test.sh: diff/cmp IN vs OUT

--- test.sh
+++ test.sh
@@ -17,91 +17,91 @@ set -x
 
 # simple: DVI -> dump (file)
 $DVIASM $IN/test.dvi -o $OUT/testo.dump
-diff $OUT/testo.dump $IN/test.dump || exit 1
+diff $IN/test.dump $OUT/testo.dump || exit 1
 $DVIASM $IN/test-ja.dvi -o $OUT/testo-ja.dump
-diff $OUT/testo-ja.dump $IN/test-ja.dump || exit 1
+diff $IN/test-ja.dump $OUT/testo-ja.dump || exit 1
 $DVIASM -p $IN/test-ja-p.dvi -o $OUT/testo-ja-p.dump
-diff $OUT/testo-ja-p.dump $IN/test-ja-p.dump || exit 1
+diff $IN/test-ja-p.dump $OUT/testo-ja-p.dump || exit 1
 
 # simple: DVI -> dump (stdout)
 $DVIASM $IN/test.dvi >$OUT/test.dump
-diff $OUT/test.dump $IN/test.dump || exit 1
+diff $IN/test.dump $OUT/test.dump || exit 1
 $DVIASM $IN/test-ja.dvi >$OUT/test-ja.dump
-diff $OUT/test-ja.dump $IN/test-ja.dump || exit 1
+diff $IN/test-ja.dump $OUT/test-ja.dump || exit 1
 $DVIASM -p $IN/test-ja-p.dvi >$OUT/test-ja-p.dump
-diff $OUT/test-ja-p.dump $IN/test-ja-p.dump || exit 1
+diff $IN/test-ja-p.dump $OUT/test-ja-p.dump || exit 1
 
 # simple: dump -> DVI (file)
 $DVIASM -o $OUT/test.dump.dvi $IN/test.dump
-cmp $OUT/test.dump.dvi $IN/test.dump.dvi || exit 1
+cmp $IN/test.dump.dvi $OUT/test.dump.dvi || exit 1
 $DVIASM -o $OUT/test-ja.dump.dvi $IN/test-ja.dump
-cmp $OUT/test-ja.dump.dvi $IN/test-ja.dump.dvi || exit 1
+cmp $IN/test-ja.dump.dvi $OUT/test-ja.dump.dvi || exit 1
 $DVIASM -p -o $OUT/test-ja-p.dump.dvi $IN/test-ja-p.dump
-cmp $OUT/test-ja-p.dump.dvi $IN/test-ja-p.dump.dvi || exit 1
+cmp $IN/test-ja-p.dump.dvi $OUT/test-ja-p.dump.dvi || exit 1
 
 # simple: dump -> DVI (stdout)
 $DVIASM $IN/test.dump >$OUT/testo.dump.dvi
-cmp $OUT/testo.dump.dvi $IN/test.dump.dvi || exit 1
+cmp $IN/test.dump.dvi $OUT/testo.dump.dvi || exit 1
 $DVIASM $IN/test-ja.dump >$OUT/testo-ja.dump.dvi
-cmp $OUT/testo-ja.dump.dvi $IN/test-ja.dump.dvi || exit 1
+cmp $IN/test-ja.dump.dvi $OUT/testo-ja.dump.dvi || exit 1
 $DVIASM -p $IN/test-ja-p.dump >$OUT/testo-ja-p.dump.dvi
-cmp $OUT/testo-ja-p.dump.dvi $IN/test-ja-p.dump.dvi || exit 1
+cmp $IN/test-ja-p.dump.dvi $OUT/testo-ja-p.dump.dvi || exit 1
 
 ##### from https://github.com/aminophen/tex-assort
 
 # font/native: DVI/XDV -> dump
 $DVIASM $IN/font.dvi -o $OUT/font.dump
-diff $OUT/font.dump $IN/font.dump || exit 1
+diff $IN/font.dump $OUT/font.dump || exit 1
 $DVIASM $IN/font.xdv -o $OUT/font.xdump
-diff $OUT/font.xdump $IN/font.xdump || exit 1
+diff $IN/font.xdump $OUT/font.xdump || exit 1
 $DVIASM $IN/native.xdv -o $OUT/native.xdump
-diff $OUT/native.xdump $IN/native.xdump || exit 1
+diff $IN/native.xdump $OUT/native.xdump || exit 1
 
 # font/native: dump -> DVI/XDV
 $DVIASM $IN/font.dump -o $OUT/font.dump.dvi
-cmp $OUT/font.dump $IN/font.dump || exit 1
+cmp $IN/font.dump $OUT/font.dump || exit 1
 $DVIASM $IN/font.xdump -o $OUT/font.xdump.xdv
-cmp $OUT/font.xdump.xdv $IN/font.xdump.xdv || exit 1
+cmp $IN/font.xdump.xdv $OUT/font.xdump.xdv || exit 1
 $DVIASM $IN/native.xdump -o $OUT/native.xdump.xdv
-cmp $OUT/native.xdump.xdv $IN/native.xdump.xdv || exit 1
+cmp $IN/native.xdump.xdv $OUT/native.xdump.xdv || exit 1
 
 ##### documented behavior by Jin-Hwan Cho (ChoF)
 
 # tb89cho: dump -> DVI
 $DVIASM $IN/hello.dump -o $OUT/hello.dump.dvi
-cmp $OUT/hello.dump.dvi $IN/hello.dump.dvi || exit 1
+cmp $IN/hello.dump.dvi $OUT/hello.dump.dvi || exit 1
 $DVIASM $IN/hello5.dump -o $OUT/hello5.dump.dvi
-cmp $OUT/hello5.dump.dvi $IN/hello5.dump.dvi || exit 1
+cmp $IN/hello5.dump.dvi $OUT/hello5.dump.dvi || exit 1
 $DVIASM $IN/hello6.dump -o $OUT/hello6.dump.dvi
-cmp $OUT/hello6.dump.dvi $IN/hello6.dump.dvi || exit 1
+cmp $IN/hello6.dump.dvi $OUT/hello6.dump.dvi || exit 1
 
 # ajt0201cho: DVI -> dump
 $DVIASM $IN/ajt01lig.dvi >$OUT/ajt01lig.dump
-diff $OUT/ajt01lig.dump $IN/ajt01lig.dump || exit 1
+diff $IN/ajt01lig.dump $OUT/ajt01lig.dump || exit 1
 $DVIASM --ptex $IN/ajt03jp.dvi >$OUT/ajt03jp.dump
-diff $OUT/ajt03jp.dump $IN/ajt03jp.dump || exit 1
+diff $IN/ajt03jp.dump $OUT/ajt03jp.dump || exit 1
 
 # ajt0201cho: dump -> DVI
 ## ajt05jt.dump -> ajt05jt.dump.dvi was wrong (DVIV_ID)
 ## => fixed in py3-20191126 (#10, #11)
 $DVIASM $IN/ajt01lig.dump -o $OUT/ajt01lig.dump.dvi
-cmp $OUT/ajt01lig.dump.dvi $IN/ajt01lig.dump.dvi || exit 1
+cmp $IN/ajt01lig.dump.dvi $OUT/ajt01lig.dump.dvi || exit 1
 $DVIASM $IN/ajt02omega.dump -o $OUT/ajt02omega.dump.dvi
-cmp $OUT/ajt02omega.dump.dvi $IN/ajt02omega.dump.dvi || exit 1
+cmp $IN/ajt02omega.dump.dvi $OUT/ajt02omega.dump.dvi || exit 1
 $DVIASM --ptex $IN/ajt03jp.dump -o $OUT/ajt03jp.dump.dvi
-cmp $OUT/ajt03jp.dump.dvi $IN/ajt03jp.dump.dvi || exit 1
+cmp $IN/ajt03jp.dump.dvi $OUT/ajt03jp.dump.dvi || exit 1
 $DVIASM --ptex $IN/ajt04jy.dump -o $OUT/ajt04jy.dump.dvi
-cmp $OUT/ajt04jy.dump.dvi $IN/ajt04jy.dump.dvi || exit 1
+cmp $IN/ajt04jy.dump.dvi $OUT/ajt04jy.dump.dvi || exit 1
 $DVIASM --ptex $IN/ajt05jt.dump -o $OUT/ajt05jt.dump.dvi
-cmp $OUT/ajt05jt.dump.dvi $IN/ajt05jt.dump.dvi || exit 1
+cmp $IN/ajt05jt.dump.dvi $OUT/ajt05jt.dump.dvi || exit 1
 $DVIASM --subfont=outbtm,outgtm, $IN/ajt06kr.dump -o $OUT/ajt06kr.dump.dvi
-cmp $OUT/ajt06kr.dump.dvi $IN/ajt06kr.dump.dvi || exit 1
+cmp $IN/ajt06kr.dump.dvi $OUT/ajt06kr.dump.dvi || exit 1
 
 # ajt0201cho: DVI -> dump (back)
 $DVIASM $IN/ajt02omega.dump.dvi -o $OUT/ajt02omega.dump.dump
-diff $OUT/ajt02omega.dump.dump $IN/ajt02omega.dump.dump || exit 1
+diff $IN/ajt02omega.dump.dump $OUT/ajt02omega.dump.dump || exit 1
 $DVIASM --subfont=outbtm,outgtm, $IN/ajt06kr.dump.dvi -o $OUT/ajt06kr.dump.dump
-diff $OUT/ajt06kr.dump.dump $IN/ajt06kr.dump.dump || exit 1
+diff $IN/ajt06kr.dump.dump $OUT/ajt06kr.dump.dump || exit 1
 
 ##### from GitHub PR 5 by Masamichi Hosoda
 
@@ -110,7 +110,7 @@ diff $OUT/ajt06kr.dump.dump $IN/ajt06kr.dump.dump || exit 1
 ## => fixed in py2-20190202 (#3, #5)
 $DVIASM -u sp $IN/checklength.dump -o $OUT/checklength.dump.dvi
 $DVIASM -u sp $OUT/checklength.dump.dvi >$OUT/checklength.dump.dump
-diff $OUT/checklength.dump.dump $IN/checklength.dump || exit 1
+diff $IN/checklength.dump $OUT/checklength.dump.dump || exit 1
 
 ##### from GitHub Issue 6/7
 
@@ -118,9 +118,9 @@ diff $OUT/checklength.dump.dump $IN/checklength.dump || exit 1
 ## set3 was unsupported in Python 2.x
 ## => fixed in py3-20191126 (#6, #7)
 $DVIASM $IN/overbmp-up.dvi >$OUT/overbmp-up.dump
-diff $OUT/overbmp-up.dump $IN/overbmp-up.dump || exit 1
+diff $IN/overbmp-up.dump $OUT/overbmp-up.dump || exit 1
 $DVIASM $IN/overbmp-up.dump -o $OUT/overbmp-up.dump.dvi
-cmp $OUT/overbmp-up.dump.dvi $IN/overbmp-up.dump.dvi || exit 1
+cmp $IN/overbmp-up.dump.dvi $OUT/overbmp-up.dump.dvi || exit 1
 
 ##### from GitHub Issue 12
 
@@ -128,27 +128,27 @@ cmp $OUT/overbmp-up.dump.dvi $IN/overbmp-up.dump.dvi || exit 1
 ## gh0012.dvi -> gh0012.dump caused error with --ptex
 ## => [TODO] workaround in py3-20200905, needs reconsider (#13)
 $DVIASM $IN/gh0012.dvi >$OUT/gh0012.dump
-diff $OUT/gh0012.dump $IN/gh0012.dump || exit 1
+diff $IN/gh0012.dump $OUT/gh0012.dump || exit 1
 $DVIASM --ptex $IN/gh0012.dvi >$OUT/gh0012-p.dump
-diff $OUT/gh0012-p.dump $IN/gh0012.dump || exit 1
+diff $IN/gh0012.dump $OUT/gh0012-p.dump || exit 1
 
 # gh0012: dump -> DVI
 ## gh0012.dump -> gh0012.dump.dvi caused error with --ptex
 ## => [TODO] workaround in py3-20200905, needs reconsider (#13)
 $DVIASM $IN/gh0012.dump -o $OUT/gh0012.dump.dvi
-cmp $OUT/gh0012.dump.dvi $IN/gh0012.dump.dvi || exit 1
+cmp $IN/gh0012.dump.dvi $OUT/gh0012.dump.dvi || exit 1
 $DVIASM --ptex $IN/gh0012.dump -o $OUT/gh0012-p.dump.dvi
-cmp $OUT/gh0012-p.dump.dvi $IN/gh0012.dump.dvi || exit 1
+cmp $IN/gh0012.dump.dvi $OUT/gh0012-p.dump.dvi || exit 1
 
 ##### user-defined subfont
 
 # ipxm: DVI -> dump
 $DVIASM --subfont=ipxm-r-u,ipxg-r-u, $IN/ipxm.dvi >$OUT/ipxm.dump
-diff $OUT/ipxm.dump $IN/ipxm.dump || exit 1
+diff $IN/ipxm.dump $OUT/ipxm.dump || exit 1
 
 # ipxm: dump -> DVI
 $DVIASM --subfont=ipxm-r-u,ipxg-r-u, $IN/ipxm.dump -o $OUT/ipxm.dump.dvi
-cmp $OUT/ipxm.dump.dvi $IN/ipxm.dump.dvi || exit 1
+cmp $IN/ipxm.dump.dvi $OUT/ipxm.dump.dvi || exit 1
 
 ##### inputenc and fontenc
 
@@ -156,17 +156,17 @@ cmp $OUT/ipxm.dump.dvi $IN/ipxm.dump.dvi || exit 1
 ## varenc-p.dvi -> varenc-p.dump caused error with --ptex
 ## => [TODO] workaround in py3-20200905, needs reconsider (#13)
 $DVIASM -p $IN/varenc-p.dvi >$OUT/varenc-p.dump
-diff $OUT/varenc-p.dump $IN/varenc-p.dump || exit 1
+diff $IN/varenc-p.dump $OUT/varenc-p.dump || exit 1
 $DVIASM $IN/varenc-up.dvi >$OUT/varenc-up.dump
-diff $OUT/varenc-up.dump $IN/varenc-up.dump || exit 1
+diff $IN/varenc-up.dump $OUT/varenc-up.dump || exit 1
 
 # varenc: dump -> DVI
 ## varenc-p.dump -> varenc-p.dump.dvi caused error with --ptex
 ## => [TODO] workaround in py3-20200905, needs reconsider (#13)
 $DVIASM -p $IN/varenc-p.dump -o $OUT/varenc-p.dump.dvi
-cmp $OUT/varenc-p.dump.dvi $IN/varenc-p.dump.dvi || exit 1
+cmp $IN/varenc-p.dump.dvi $OUT/varenc-p.dump.dvi || exit 1
 $DVIASM $IN/varenc-up.dump -o $OUT/varenc-up.dump.dvi
-cmp $OUT/varenc-up.dump.dvi $IN/varenc-up.dump.dvi || exit 1
+cmp $IN/varenc-up.dump.dvi $OUT/varenc-up.dump.dvi || exit 1
 
 ##### command-line encoding option
 
@@ -179,23 +179,23 @@ cmp $OUT/varenc-up.dump.dvi $IN/varenc-up.dump.dvi || exit 1
 
 # first, ordinary UTF-8 dump/compile
 $DVIASM -p $IN/jisx0208.dvi -o $OUT/jisx0208.dump
-diff $OUT/jisx0208.dump $IN/jisx0208.dump || exit 1
+diff $IN/jisx0208.dump $OUT/jisx0208.dump || exit 1
 $DVIASM -p $IN/jisx0208.dump -o $OUT/jisx0208.dump.dvi
-cmp $OUT/jisx0208.dump.dvi $IN/jisx0208.dump.dvi || exit 1
+cmp $IN/jisx0208.dump.dvi $OUT/jisx0208.dump.dvi || exit 1
 
 # next, Shift-JIS dump/compile
 $DVIASM -e sjis -p $IN/jisx0208.dvi -o $OUT/jisx0208-sjp.dump
 iconv -f UTF-8 -t SHIFT-JIS $IN/jisx0208.dump >$OUT/jisx0208-s.dump
-diff $OUT/jisx0208-sjp.dump $OUT/jisx0208-s.dump || exit 1
+diff $OUT/jisx0208-s.dump $OUT/jisx0208-sjp.dump || exit 1
 $DVIASM -e sjis -p $OUT/jisx0208-sjp.dump -o $OUT/jisx0208-sjp.dump.dvi
-cmp $OUT/jisx0208-sjp.dump.dvi $IN/jisx0208.dump.dvi || exit 1
+cmp $IN/jisx0208.dump.dvi $OUT/jisx0208-sjp.dump.dvi || exit 1
 
 # then, EUC-JP dump/compile
 $DVIASM -e eucjp -p $IN/jisx0208.dvi -o $OUT/jisx0208-ejp.dump
 iconv -f UTF-8 -t EUC-JP $IN/jisx0208.dump >$OUT/jisx0208-e.dump
-diff $OUT/jisx0208-ejp.dump $OUT/jisx0208-e.dump || exit 1
+diff $OUT/jisx0208-e.dump $OUT/jisx0208-ejp.dump || exit 1
 $DVIASM -e eucjp -p $OUT/jisx0208-ejp.dump -o $OUT/jisx0208-ejp.dump.dvi
-cmp $OUT/jisx0208-ejp.dump.dvi $IN/jisx0208.dump.dvi || exit 1
+cmp $IN/jisx0208.dump.dvi $OUT/jisx0208-ejp.dump.dvi || exit 1
 
 #####
 
